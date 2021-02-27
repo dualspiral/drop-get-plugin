be584e86863c377a819887e28f15fcf337fe162d
GitHub Action <action@github.com>
2021-02-27 11:36:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/dualspiral/dropgetplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dualspiral/dropgetplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.dualspiral.dropgetplugin.services
 
+import com.github.dualspiral.dropgetplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
